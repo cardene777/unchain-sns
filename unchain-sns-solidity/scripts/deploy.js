b478e937bdf566810ee04f1cc5d27b5774e32d29
heku777 <chotips08@gmail.com>
2022-09-08 08:15:49 +0900

    💫 create unchain app

--- unchain-sns-solidity/scripts/deploy.js
+++ unchain-sns-solidity/scripts/deploy.js
@@ -1,71 +1,24 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
-  const UnchainSnsFactory = await hre.ethers.getContractFactory("Sns");
-  const UnchainSnsContract = await UnchainSnsFactory.deploy();
-  const UnchainSns = await UnchainSnsContract.deployed();
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+    const UnchainSnsFactory = await hre.ethers.getContractFactory("Sns");
+    const UnchainSnsContract = await UnchainSnsFactory.deploy();
+    const UnchainSns = await UnchainSnsContract.deployed();
 
-  console.log("Contract deployed to: ", UnchainSns.address);
-  console.log("owner address: ", owner.address);
-  console.log("randomPerson address: ", randomPerson.address);
-
-  await UnchainSnsContract.post("test1")
-
-  let postData
-  postData = await UnchainSnsContract.getPost(1)
-  console.log("*".repeat(50));
-  console.log("postData: ", postData);
-  console.log("*".repeat(50));
-
-  await UnchainSnsContract.post("test2")
-
-  postData = await UnchainSnsContract.getPost(2)
-  console.log("*".repeat(50));
-  console.log("postData: ", postData);
-  console.log("*".repeat(50));
-
-  try {
-      await UnchainSnsContract.goodPost(1)
-  } catch (e) {
-      console.log(e.message);
-  }
-
-  await UnchainSnsContract.connect(randomPerson).goodPost(1)
-
-  postData = await UnchainSnsContract.getPost(1)
-  console.log("*".repeat(50));
-  console.log("postData: ", postData);
-  console.log("*".repeat(50));
-
-  try {
-      await UnchainSnsContract.connect(randomPerson).goodPost(1)
-  } catch (e) {
-      console.log(e.message);
-  }
-
-  await UnchainSnsContract.connect(randomPerson).goodRemove(1)
-
-  postData = await UnchainSnsContract.getPost(1)
-  console.log("*".repeat(50));
-  console.log("postData: ", postData);
-  console.log("*".repeat(50));
-
-  try {
-      await UnchainSnsContract.connect(randomPerson).goodRemove(1)
-  } catch (e) {
-      console.log(e.message);
-  }
-
-
+    console.log("Contract deployed to: ", UnchainSns.address);
+    console.log("owner address: ", owner.address);
+    console.log("owner Balance: ", UnchainSns.balance);
 };
 
 const runMain = async () => {
-  try {
-      await main();
-      process.exit(0);
-  } catch (error) {
-      console.log(error);
-      process.exit(1);
-  }
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
 };
 
 runMain();
+
+// 0x81760FBD4554d75383fc7ecd56Ed5dA1C1C83d51
